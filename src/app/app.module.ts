a546b204e96859f62895596df3ae0a7cbf262f61
Greg <greg.prill@yourbrandlive.com>
2017-05-31 23:56:39 -0700

    full resize functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Ng2UploaderModule } from 'ng2-uploader';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ 
+  	BrowserModule,
+  	Ng2UploaderModule,
+  	FormsModule,
+  	HttpModule
+  ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
